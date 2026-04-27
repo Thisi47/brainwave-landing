4ff32682a76afa43916009c23561c9886ba57ed6
Thisi47 <150513044+Thisi47@users.noreply.github.com>
2026-04-27 16:03:50 +0100

    chore: nettoyage de la page d'accueil

---
+++ src/features/hero/HeroSection.tsx
@@ -0,0 +1,9 @@
+export const HeroSection = () => {
+    return (
+        <section className="py-20 px-6 text-center">
+            <h1 className="text-5xl font-bold">
+                Manage your team with Brainwave
+            </h1>
+        </section>
+    );
+}
